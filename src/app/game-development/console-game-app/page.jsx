4c6327f05437f763d09d4c11129d84e1d4af4196
waleedali7825-ship-it <waleedali7825@gmail.com>
2026-03-11 01:19:23 +0500

    content

--- src/app/game-development/console-game-app/page.jsx
+++ src/app/game-development/console-game-app/page.jsx
@@ -33,7 +33,7 @@ const bannerData = {
   bgImage: "/images/front-end-banner.webp",
   heading1: (
     <h1 className="same-heading">
-      Expert<span> Google & Meta </span> Ads Management Services
+      Expert<span> Google & Meta Ads </span> Management Services
     </h1>
   ),
   para: (
