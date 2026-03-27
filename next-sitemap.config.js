d5be258e075c0485d32c6ff83114cc1aa46c0b3c
waleedali7825-ship-it <waleedali7825@gmail.com>
2026-03-27 22:06:35 +0500

    chnage

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   siteUrl: 'https://www.hamkoders.com',
   generateRobotsTxt: true,
+
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+    await config.transform(config, '/about'),
+    await config.transform(config, '/services'),
+    await config.transform(config, '/contact-us'),
+    await config.transform(config, '/industries'),
+    await config.transform(config, '/portfolio'),
+  ],
 };
