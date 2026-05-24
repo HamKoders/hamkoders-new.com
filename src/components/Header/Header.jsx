d88bc5c646cf356f264643c1f38d36546239b06c
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-05-25 00:40:32 +0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -594,6 +594,9 @@ const Header = () => {
                                <button onClick={() => translatePage('de')}>DE</button> 
                      </div>
                   </div> */}
+                  <div className="header-right-btn">
+                  <Link href={"/contact-us"}>Contact Us</Link>
+                  </div>
                    
                 </div>
               </div>
