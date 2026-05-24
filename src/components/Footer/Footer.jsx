e6ea68c2277821628117c6821814445ac0c53a69
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-05-24 21:35:24 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -196,7 +196,7 @@ const currentYear = currentDate.getFullYear();
   <li>
   <div className="location-item">
     <Link href={"/"} className="location-item">
-    <Image src={usFlag} alt="pak-flag" width={35} height={23} />
+    <Image src={usFlag} alt="pak-flag" width={45} height={28} />
     <span>
       <strong>Head Office</strong>
       <em>Karachi, Pakistan</em>
