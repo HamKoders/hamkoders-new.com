69ed62071829b2310db2c910287ae825d9719b8c
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-05-25 00:49:20 +0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
     <>
      <div className="gtranslate_wrapper"></div>
 <Script id="gtranslate-settings" strategy="afterInteractive">
-  {`window.gtranslateSettings = {"default_language":"en","languages":["en","de"],"wrapper_selector":".gtranslate_wrapper","flag_size":16,"switcher_horizontal_position":"inline","flag_style":"3d"}`}
+  {`window.gtranslateSettings = {"default_language":"en","languages":["en","de"],"wrapper_selector":".gtranslate_wrapper","flag_size":16,"switcher_horizontal_position":"right","switcher_vertical_position":"top","flag_style":"3d"}`}
 </Script>
 <Script 
   src="https://cdn.gtranslate.net/widgets/latest/dwf.js"
