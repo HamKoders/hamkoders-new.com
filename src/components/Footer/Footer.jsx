1d1e381c6bbd75c060c00e1cc3ce78a2e0b02416
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-05-24 20:06:38 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -275,7 +275,7 @@ const currentYear = currentDate.getFullYear();
                 <div className="row">
                   <div className="col-lg-12">
                     <div className="footer-content footer-social ">
-                     <ul className="social-icons-row>
+                     <ul className="social-icons-row">
                         <li>
                           <Link
                             href={"https://www.facebook.com/Hamkoders/"}
