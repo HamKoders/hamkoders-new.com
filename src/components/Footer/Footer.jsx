5e24c09b2d41a8659361e866970f6aec6357dd9f
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-01 23:03:29 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -196,7 +196,7 @@ const currentYear = currentDate.getFullYear();
   <li>
   <div className="location-item">
     <Link href={"/"} className="location-item">
-<Image src={usFlag} alt="pak-flag" width={35} height={23} />
+<Image src={usFlag} alt="pak-flag" width={50} height={40} />
     <span>
       <strong>Head Office</strong>
       <em>Karachi, Pakistan</em>
@@ -207,7 +207,7 @@ const currentYear = currentDate.getFullYear();
 <li>
   <div className="location-item">
     <Link href={"/"} className="location-item">
-   <img src="https://flagcdn.com/w40/at.png" alt="austria-flag" width={35} height={23} />
+   <img src="https://flagcdn.com/w40/at.png" alt="austria-flag" width={30} height={20} />
     <span>
       <strong>European Office</strong>
       <em>Vienna, Austria</em>
