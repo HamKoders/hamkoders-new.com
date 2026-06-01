53cacd428fc6e40669b813163456b364334b6369
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-02 02:03:25 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -230,7 +230,7 @@ const currentYear = currentDate.getFullYear();
                       </p>
                     </div>
                   </div>
-                  <div className="col-lg-3 col-md-6" style={{display: 'flex', justifyContent: 'flex-end'}}>
+                 <div className="col-lg-3 col-md-6" style={{paddingLeft: '40px'}}>
                     <div className="footer-content">
                       <h3>Contact</h3>
                      <ul>
