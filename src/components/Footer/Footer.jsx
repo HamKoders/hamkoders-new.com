d5399c86fb812b5a6298418f722c81d41b509735
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-01 22:08:11 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -188,7 +188,7 @@ const currentYear = currentDate.getFullYear();
                   </div>
                 </div>
 
-                <div className="row justify-content-center align-items-center">
+                <div className="row justify-content-center align-items-center footer-bottom-row">
                   <div className="col-lg-3">
                     <div className="footer-content">
                       <h3>Location</h3>
