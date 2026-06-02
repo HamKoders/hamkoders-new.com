c8094d6e6035656b1c2a4aff44441b0b3cd0baf5
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-03 01:34:47 +0500

    Update AndroidProgress.jsx

--- src/components/AndroidProgress/AndroidProgress.jsx
+++ src/components/AndroidProgress/AndroidProgress.jsx
@@ -11,14 +11,14 @@ const AndroidProgress = () => {
               <div className="row">
                 <div className="col-lg-3 col-md-6 col-12">
                   <div className="android-progress-box">
-                    <h3>50%</h3>
+                   <h3 translate="no">50%</h3>
                     <h4>Expert Developers</h4>
                   </div>
                 </div>
                 <div className="col-lg-3 col-md-6 col-12">
                   <div className="android-progress-box">
                     <div className="android-progress-box">
-                      <h3>100+</h3>
+                     <h3 translate="no">100+</h3>
                       <h4>Active Apps</h4>
                     </div>
                   </div>
@@ -26,7 +26,7 @@ const AndroidProgress = () => {
                 <div className="col-lg-3 col-md-6 col-12">
                   <div className="android-progress-box">
                     <div className="android-progress-box">
-                       <h3>15+</h3>
+                     <h3 translate="no">15+</h3>
                       <h4>Industries Served</h4>
                     </div>
                   </div>
@@ -34,7 +34,7 @@ const AndroidProgress = () => {
                 <div className="col-lg-3 col-md-6 col-12">
                   <div className="android-progress-box">
                     <div className="android-progress-box">
-                      <h3>75+</h3>
+                     <h3 translate="no">75+</h3>
                       <h4>Client Reviews</h4>
                     </div>
                   </div>
