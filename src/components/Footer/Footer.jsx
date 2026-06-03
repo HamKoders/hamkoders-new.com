200cc8b0d7824eef42e7228694237435da04ff53
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-04 01:02:44 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -100,7 +100,7 @@ const currentYear = currentDate.getFullYear();
                   <div className="col-lg-3 col-md-6">
                     <div className="footer-navigation">
                       <h3 style={{paddingLeft: "60px"}}>Company</h3>
-                      <ul style={{paddingLeft: "50px"}}>
+                      <ul style={{paddingLeft: "50px!important"}}>
                         <li>
                           <Link href={"/about"}>About us</Link>
                         </li>
