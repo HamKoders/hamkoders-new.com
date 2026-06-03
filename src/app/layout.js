77e576eb9eb550cdf818dae734e841ad84fa52e6
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-04 04:56:07 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,13 @@ import Script from "next/script";
 
 export const metadata = {
   title: "Design Agency for Startups | Branding, UI/UX & Web Solutions",
-  description: "We help startups grow through bold branding, UI/UX design, and smart web solutions that turn big ideas into real results. Let's bring your vision to life today.",
+  description:
+    "We help startups grow through bold branding, UI/UX design, and smart web solutions that turn big ideas into real results. Let's bring your vision to life today.",
   icons: {
-    icon: [{ url: "/logo.svg" }, { url: "/logo.svg", type: "image/svg+xml" }],
+    icon: [
+      { url: "/logo.svg" },
+      { url: "/logo.svg", type: "image/svg+xml" },
+    ],
     apple: "/logo.svg",
     shortcut: "/logo.svg",
   },
@@ -16,14 +20,29 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning>
       <body>
         <BootstrapClient />
+
         {children}
+
         <TawkChat />
+
+        {/* GTranslate Settings */}
         <Script id="gtranslate-settings" strategy="beforeInteractive">
-          {`window.gtranslateSettings = {"default_language":"en","languages":["en","de"],"wrapper_selector":".gtranslate_wrapper","flag_size":16,"switcher_horizontal_position":"inline","flag_style":"3d"}`}
+          {`
+            window.gtranslateSettings = {
+              default_language: "en",
+              languages: ["en", "de"],
+              wrapper_selector: ".gtranslate_wrapper",
+              flag_size: 16,
+              switcher_horizontal_position: "inline",
+              flag_style: "3d"
+            };
+          `}
         </Script>
+
+        {/* GTranslate Script */}
         <Script
           src="https://cdn.gtranslate.net/widgets/latest/dwf.js"
           strategy="afterInteractive"
