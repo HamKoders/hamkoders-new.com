d4a0e336b9a048955e937cc5f5da221fbe5a8c4c
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-04 04:41:58 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import BootstrapClient from "@/components/BootstrapClient";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TawkChat from "@/components/TawkChat";
-import Script from "next/script";
 
 export const metadata = {
   title: "Design Agency for Startups | Branding, UI/UX & Web Solutions",
@@ -21,13 +20,6 @@ export default function RootLayout({ children }) {
         <BootstrapClient />
         {children}
         <TawkChat />
-        <Script id="gtranslate-settings" strategy="beforeInteractive">
-          {`window.gtranslateSettings = {"default_language":"en","languages":["en","de"],"wrapper_selector":".gtranslate_wrapper","flag_size":16,"switcher_horizontal_position":"inline","flag_style":"3d"}`}
-        </Script>
-        <Script
-          src="https://cdn.gtranslate.net/widgets/latest/dwf.js"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
