92ab1d455308620f37236aa6eac8b4dffbd70531
muhammadhammad0810 <muhammadhammad0810@gmail.com>
2026-06-04 04:25:41 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,9 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TawkChat from "@/components/TawkChat";
 
-
 export const metadata = {
   title: "Design Agency for Startups | Branding, UI/UX & Web Solutions",
-  description: "We help startups grow through bold branding, UI/UX design, and smart web solutions that turn big ideas into real results. Let’s bring your vision to life today.",
+  description: "We help startups grow through bold branding, UI/UX design, and smart web solutions that turn big ideas into real results. Let's bring your vision to life today.",
   icons: {
     icon: [{ url: "/logo.svg" }, { url: "/logo.svg", type: "image/svg+xml" }],
     apple: "/logo.svg",
@@ -17,6 +16,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.gtranslateSettings = {"default_language":"en","languages":["en","de"],"wrapper_selector":".gtranslate_wrapper","flag_size":16,"switcher_horizontal_position":"inline","flag_style":"3d"}`
+          }}
+        />
+        <script src="https://cdn.gtranslate.net/widgets/latest/dwf.js" defer></script>
+      </head>
       <body>
         <BootstrapClient />
         {children}
